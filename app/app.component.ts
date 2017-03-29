56eeb8cca4fdee310f0f45fd89181112adb5af08
alexandra holcombe and grinil khanna <alexandra+grinil@>
2017-03-29 14:51:01 -0700

    Create keg-list component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,15 +1,27 @@
 import { Component } from '@angular/core';
+import { Keg } from './keg.model'
 
 @Component({
   selector: 'my-app',
   template: `
   <div class="container">
-  <h1>Todo List</h1>
+  <h1>Tapster</h1>
+    <keg-list [childKegList]="parentKegList"></keg-list>
   </div>
-  `
+  `,
+  styles: [`
+    .keg {
+      outline: 1px solid red;
+    }
+  `]
 })
 
 
 export class AppComponent {
+  parentKegList: Keg[] = [
+    new Keg('Mongo', 'Port Brewing', 7, 7.5),
+    new Keg('Talon', 'Mendocino Brewing Co', 7, 10),
+    new Keg('Wolf Mother', 'Golden Road', 9, 11.3)
+  ];
 
 }
---
+++ app/keg-list.component.ts
@@ -0,0 +1,34 @@
+import { Component, Input } from '@angular/core';
+import { Keg } from './keg.model';
+
+@Component({
+  selector: 'keg-list',
+  template: `
+  <div class="keg-list">
+    <h2>Keg List</h2>
+
+    <div class="keg" *ngFor="let currentKeg of childKegList">
+      <h3 class="name">{{currentKeg.name}}</h3>
+      <h4 class="brand"> {{currentKeg.brand}}</h4>
+      <p>$<span class="price">{{currentKeg.price}}</span></p>
+      <p><span class="abv">{{currentKeg.abv}}</span>% abv</p>
+    </div>
+  </div>
+  `,
+  styles: [`
+    .keg {
+      display: inline-block;
+      padding: 10px;
+      margin: 10px;
+      outline: 1px solid red;
+      width: 20%;
+      text-align: center;
+    }
+  `]
+})
+
+
+export class KegListComponent {
+  @Input() childKegList: Keg[];
+
+}
