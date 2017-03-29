53a70b4bc11818d6fc9398666072aca60f91176b
alexandra holcombe and grinil khanna <alexandra+grinil@>
2017-03-29 13:55:23 -0700

    Create initial files and directories

---
+++ app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `
+  <div class="container">
+  <h1>Todo List</h1>
+  </div>
+  `
+})
+
+
+export class AppComponent {
+
+}
